6461d502e178c2f5b227c8fa6043bd685c4ffb85
Dylan Wilson <dylan@craftworkgames.com>
2015-01-20 23:16:29 +1000

    added nuspec file for packaging to nuget

--- Astrid.Framework/Properties/AssemblyInfo.cs
+++ Astrid.Framework/Properties/AssemblyInfo.cs
@@ -1,17 +1,15 @@
 ﻿using System.Resources;
 using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Astrid.Framework")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Cross-platform open source C# game-development framework")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Craftwork Games")]
 [assembly: AssemblyProduct("Astrid.Framework")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright © 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +24,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
