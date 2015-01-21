f633b035c0e3159b7a2ee25291a2670088af1ea1
Dylan Wilson <dylan@craftworkgames.com>
2015-01-21 23:15:20 +1000

    changed the nuspec file to allow the framework to target all supported platforms

--- PublishNuGetPackage.ps1
+++
@@ -1,5 +0,0 @@
-$ProjectPath = ".\Astrid.Windows\Astrid.Windows.csproj"
-.\.nuget\NuGet Pack "$ProjectPath" -OutputDirectory "Builds" -IncludeReferencedProjects -Properties Configuration=Release
-
-$Package = gci .\Builds | sort LastWriteTime | select -Last 1
-.\.nuget\NuGet Push .\Builds\$Package
