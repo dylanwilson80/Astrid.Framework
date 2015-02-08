421f54ca672a6d0c0eae176f23bfdf8ece7204e1
Dylan Wilson <dylan@craftworkgames.com>
2015-02-08 22:12:34 +1000

    started work on animation code

---
+++ Astrid.Framework.Tests/Animations/AnimationTests.cs
@@ -0,0 +1,32 @@
+﻿using Astrid.Framework.Animations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Astrid.Framework.Tests.Animations
+{
+    [TestClass]
+    public class AnimationTests
+    {
+        [TestMethod]
+        public void Animation_Update_Test()
+        {
+            var animation = new Animation(duration: 2.0f);
+
+            Assert.AreEqual(0.0f, animation.CurrentValue);
+
+            animation.Update(1.0f);
+            Assert.AreEqual(0.5f, animation.CurrentValue);
+
+            animation.Update(2.0f);
+            Assert.AreEqual(1.0f, animation.CurrentValue);
+        }
+
+        [TestMethod]
+        public void Animation_Update_BoundedLessThanOrEqualToOne_Test()
+        {
+            var animation = new Animation(duration: 2.0f);
+
+            animation.Update(2.1f);
+            Assert.AreEqual(1.0f, animation.CurrentValue);
+        }
+    }
+}
