c2a2a61972a6144d7ee3fa12c6a3cd54920188da
Dylan Wilson <dylan@craftworkgames.com>
2015-02-09 23:17:27 +1000

    fixed broken animation tests

--- Astrid.Framework.Tests/Animations/AnimationTests.cs
+++ Astrid.Framework.Tests/Animations/AnimationTests.cs
@@ -1,4 +1,5 @@
-﻿using Astrid.Framework.Animations;
+﻿using System.Runtime.Remoting.Messaging;
+using Astrid.Framework.Animations;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Astrid.Framework.Tests.Animations
@@ -9,24 +10,83 @@ namespace Astrid.Framework.Tests.Animations
         [TestMethod]
         public void Animation_Update_Test()
         {
-            var animation = new Animation(duration: 2.0f);
+            var targetValue = 1.0f;
+            var animation = new FloatAnimation(2.0f, 3.0f, a => targetValue = a, duration: 2.0f);
 
-            Assert.AreEqual(0.0f, animation.CurrentValue);
+            animation.Update(0.0f);
+            Assert.AreEqual(2.0f, targetValue);
 
             animation.Update(1.0f);
-            Assert.AreEqual(0.5f, animation.CurrentValue);
+            Assert.AreEqual(2.5f, targetValue);
 
             animation.Update(2.0f);
-            Assert.AreEqual(1.0f, animation.CurrentValue);
+            Assert.AreEqual(3.0f, targetValue);
         }
 
         [TestMethod]
-        public void Animation_Update_BoundedLessThanOrEqualToOne_Test()
+        public void Animation_Update_IsCompleteAfterDuration_Test()
         {
-            var animation = new Animation(duration: 2.0f);
+            var targetValue = 1.0f;
+            var animation = new FloatAnimation(2.0f, 3.0f, a => targetValue = a, duration: 2.0f);
+
+            animation.Update(2.0f);
+            Assert.IsTrue(animation.IsComplete);
 
             animation.Update(2.1f);
-            Assert.AreEqual(1.0f, animation.CurrentValue);
+            Assert.AreEqual(3.0, targetValue);
+        }
+
+        [TestMethod]
+        public void Animation_IsComplete_IfStopped_Test()
+        {
+            // ReSharper disable once NotAccessedVariable
+            var targetValue = 1.0f;
+            var animation = new FloatAnimation(2.0f, 3.0f, a => targetValue = a, duration: 2.0f);
+
+            animation.Stop();
+            Assert.IsTrue(animation.IsComplete);
+        }
+
+        [TestMethod]
+        public void Animation_IsPaused_AfterPause_Test()
+        {
+            // ReSharper disable once NotAccessedVariable
+            var targetValue = 1.0f;
+            var animation = new FloatAnimation(2.0f, 3.0f, a => targetValue = a, duration: 2.0f);
+
+            animation.Pause();
+            Assert.IsTrue(animation.IsPaused);
+        }
+        
+        [TestMethod]
+        public void Animation_IsPaused_PreventsChange_Test()
+        {
+            // ReSharper disable once NotAccessedVariable
+            var targetValue = 1.0f;
+            var animation = new FloatAnimation(2.0f, 3.0f, a => targetValue = a, duration: 2.0f);
+
+            animation.Update(1.0f);
+            Assert.AreEqual(1.0f, animation.CurrentTime);
+
+            animation.Pause();
+            animation.Update(1.0f);
+            Assert.AreEqual(1.0f, animation.CurrentTime);
+            Assert.IsFalse(animation.IsComplete);
+        }
+
+
+        [TestMethod]
+        public void Animation_Update_CustomEasingFunction_Test()
+        {
+            // ReSharper disable once NotAccessedVariable
+            var targetValue = 1.0f;
+            var animation = new FloatAnimation(2.0f, 3.0f, a => targetValue = a, duration: 2.0f)
+            {
+                EasingFunction = a => 1.2f
+            };
+
+            animation.Update(1.0f);
+            Assert.AreEqual(3.2f, targetValue);
         }
     }
 }
