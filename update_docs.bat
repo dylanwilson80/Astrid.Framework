725c3a1af72697d831c9c848131145eb91868088
Dylan Wilson <dylan@craftworkgames.com>
2015-02-14 14:52:27 +1000

    updated doc gen script

--- update_docs.bat
+++ update_docs.bat
@@ -1,7 +1,9 @@
 @echo off
+rmdir /s /q gh-pages
 git clone -b gh-pages https://github.com/craftworkgames/Astrid.Framework.git gh-pages
 rmdir /s /q .\gh-pages\documentation
 mkdir .\gh-pages\documentation
+rmdir /s /q Documentation
 mkdir Documentation
 echo "\r\n" | \SharpDox\SharpDox.Core.exe -config .\Astrid.Framework.sdox
 xcopy .\Documentation\Html\default .\gh-pages\documentation /E
