2738ebd7714e24c55fedd373407bbfe1c75564a1
Dylan Wilson <dylan@craftworkgames.com>
2015-03-11 22:27:20 +1000

    animated the elements

--- AstridDemo/Screens/TitleScreenDemo.cs
+++ AstridDemo/Screens/TitleScreenDemo.cs
@@ -22,16 +22,22 @@ namespace AstridDemo.Screens
             _target = target;
         }
 
-        public ActionManager MoveTo(Vector2 position, float duration)
+        public ActionManager MoveTo(Vector2 position, float duration, EasingFunction easingFunction)
         {
-            var animation = new Vector2Animation(_target.Position, position, v => _target.Position = v, duration);
+            var animation = new Vector2Animation(_target.Position, position, v => _target.Position = v, duration)
+            {
+                EasingFunction = easingFunction
+            };
             _animationSystem.Attach(animation);
             return this;
         }
 
-        public ActionManager RotateTo(float rotation, float duration)
+        public ActionManager RotateTo(float rotation, float duration, EasingFunction easingFunction)
         {
-            var animation = new FloatAnimation(_target.Rotation, rotation, r => _target.Rotation = r, duration);
+            var animation = new FloatAnimation(_target.Rotation, rotation, r => _target.Rotation = r, duration)
+            {
+                EasingFunction = easingFunction
+            };
             _animationSystem.Attach(animation);
             return this;
         }
@@ -57,14 +63,14 @@ namespace AstridDemo.Screens
             {
                 Text = "Welcome to", 
                 TextColor = new Color(81, 32, 0),
-                Position = new Vector2(400, 45)
+                Position = new Vector2(400, -45)
             };
             guiLayer.Controls.Add(label);
 
             var logoTexture = AssetManager.Load<Texture>("AstridLogo.png");
             var image = new GuiImage(logoTexture)
             {
-                Position = new Vector2(400, 100),
+                Position = new Vector2(400, -100),
                 
             };
             guiLayer.Controls.Add(image);
@@ -83,8 +89,18 @@ namespace AstridDemo.Screens
 
             var actionManager = new ActionManager(_animationSystem, playButton);
             actionManager
-                .MoveTo(new Vector2(400, 240), 2.0f)
-                .RotateTo(0, 2.0f);
+                .MoveTo(new Vector2(400, 240), 1.0f, EasingFunctions.CubicEaseInOut)
+                .RotateTo(0, 1.0f, EasingFunctions.CubicEaseInOut);
+
+            var actionManager2 = new ActionManager(_animationSystem, label);
+            actionManager2
+                .MoveTo(new Vector2(400, 45), 1.0f, EasingFunctions.CubicEaseInOut)
+                .RotateTo(0, 1.0f, EasingFunctions.CubicEaseInOut);
+
+            var actionManager3 = new ActionManager(_animationSystem, image);
+            actionManager3
+                .MoveTo(new Vector2(400, 100), 1.0f, EasingFunctions.CubicEaseInOut)
+                .RotateTo(0, 1.0f, EasingFunctions.CubicEaseInOut);
 
             base.Show();
         }
